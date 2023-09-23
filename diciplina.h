c8e9c9a2e2c56eb467c9a3576fc57031c535018d
Matheusdnf <matheusdiniz870@gmail.com>
2023-09-23 08:38:15 -0300

    .h do módulo diciplina

---
+++ diciplina.h
@@ -0,0 +1,7 @@
+//funções do módulo diciplina
+void menu_diciplina(void);
+void cadastrar_diciplina(void);
+void buscar_diciplina(void);
+void atualizar_diciplina(void);
+void excluir_diciplina(void);
+void relatorio_diciplina(void);
