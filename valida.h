b76113c1774a06c65d65dda1ed7368a9823b9dd7
Matheusdnf <matheusdiniz870@gmail.com>
2023-10-01 19:19:21 -0300

    assinatura das funções de telefone e nome

---
+++ valida.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+//assinatura de funções que validaão o número do telefone
+void ler_telefone(char *telefone);
+int valida_telefone(char *telefone);
+//funções de valida nome
+bool eh_letra_acentuada(char c);
+void ler_nome(char *nome);
+int valida_nome(char *nome);
