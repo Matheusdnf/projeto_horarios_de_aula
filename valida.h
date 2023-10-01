f9fd464bad1a4a276d7e5beabb8bd460118da693
Matheusdnf <matheusdiniz870@gmail.com>
2023-10-01 19:20:45 -0300

    assinatura funções de cpf,diciplina e buffer

--- valida.h
+++ valida.h
@@ -6,3 +6,14 @@ int valida_telefone(char *telefone);
 bool eh_letra_acentuada(char c);
 void ler_nome(char *nome);
 int valida_nome(char *nome);
+//funções para validar cpf
+void removerCaracteresNaoNumericos(char cpf[]);
+int validarCPF(char cpf[]);
+void ler_cpf(char cpf[]);
+//funções referente a validação de email
+//limpar o buffer de entrada para quando for utilizado o scanf
+void limpar_buffer();
+//funções para validar as diciplinas
+int valida_diciplina(char *diciplina);
+void ler_diciplina(char *diciplina);
+void diciplinas(void);
