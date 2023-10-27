65eeea1da492d07e2cea20d3223c1a99adfb4ae6
Matheusdnf <matheusdiniz870@gmail.com>
2023-10-27 09:32:12 -0300

    adicionando campo de status

--- aluno.h
+++ aluno.h
@@ -4,10 +4,11 @@ typedef struct aluno {
     char nome[100];
     char telefone[15];
     char email[225];
+    char status;    // M-Matriculado N-Não encontrado I-inexistente
 } Aluno;
 //funções do módulo aluno
 void menu_aluno(void);
-void cadastrar_aluno(void);
+Aluno* cadastrar_aluno(void);
 void buscar_aluno(void);
 void atualizar_aluno(void);
 void excluir_aluno(void);
--- professor.h
+++ professor.h
@@ -4,10 +4,11 @@ typedef struct professor {
     char nome[100];
     char cpf[15];
     char email[225];
+    char status;           //A-Ativo I-Inexistente N-Não encontrado
 } Professor;
 //módulo professores
 void menu_professor(void);
-void cadastrar_professor(void);
+Professor* cadastrar_professor(void);
 void buscar_professor(void);
 void atualizar_professor(void);
 void excluir_professor(void);
