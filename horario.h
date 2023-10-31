0bc94873a806525cc4872708e5b900eea356d5ce
Matheusdnf <matheusdiniz870@gmail.com>
2023-10-31 11:10:15 -0300

    gravaçao de dados e recuperação

--- horario.h
+++ horario.h
@@ -1,13 +1,15 @@
 //struct horário
 typedef struct horario {
+    char id;
     char periodo[7];
     char dia[7];
     char tempo[3];
     char diciplina[3];
+    char status;           //A-Ativo I-Inexistente N-Não encontrado
 } Horario;
 //módulo horário de aula
 void menu_horario(void);
-void cadastrar_horario(void);
+Horario* cadastrar_horario(void);
 void buscar_horario(void);
 void atualizar_horario(void);
 void excluir_horario(void);
@@ -19,3 +21,8 @@ void ler_dia(char* dia);
 int valida_periodo(char* periodo); 
 int valida_tempo(char* tempo);
 int valida_dia(char* dia);
+//criação de id
+int criar_id_h(void);
+void gravar_h(Horario* h);
+void exibir_h(Horario* h);
+void lista_h(void);
