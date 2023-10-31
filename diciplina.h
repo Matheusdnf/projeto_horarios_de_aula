504851d58bce9f7530212e876b0a318ab1f6dc49
Matheusdnf <matheusdiniz870@gmail.com>
2023-10-31 11:11:59 -0300

    assinatura das funções

--- diciplina.h
+++ diciplina.h
@@ -1,11 +1,14 @@
 //struct diciplina
 typedef struct diciplina {
+    short int id;
     char nome[100]; //referente ao nome do professor
     char diciplina[3];
+    char status;           //A-Ativo I-Inexistente N-Não encontrado
+    //char status;
 } Diciplina;
 //funções do módulo diciplina
 void menu_diciplina(void);
-void cadastrar_diciplina(void);
+Diciplina* cadastrar_diciplina(void);
 void buscar_diciplina(void);
 void atualizar_diciplina(void);
 void excluir_diciplina(void);
@@ -14,3 +17,9 @@ void relatorio_diciplina(void);
 int valida_diciplina(char *diciplina);
 void ler_diciplina(char *diciplina);
 void diciplinas(void);
+//função de criar id
+int criar_id_d(void);
+//gravação e exibição de dados
+void gravardiciplina(Diciplina* dic);
+void exibir_diciplinas(Diciplina* dic);
+void listardiciplina(void);
