ce9be62d5b52580afb3b6f7522968608f2511142
Matheusdnf <matheusdiniz870@gmail.com>
2023-11-01 23:16:46 -0300

    função de remover aluno

--- aluno.c
+++ aluno.c
@@ -5,12 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Apelido Struct 
-Aluno* std;    //Estudante  
-
 void menu_aluno(void) {
     while (opc!=0){
         system("clear||cls");  
+        Aluno* std;
         printf("\n");
         printf("===========================================================\n");
         printf("     ****************  Menu Aluno ******************       \n");
@@ -54,6 +52,7 @@ void menu_aluno(void) {
 
 Aluno* cadastrar_aluno(void) {
     system("clear||cls"); 
+    Aluno* std;
     std=(Aluno*)malloc(sizeof(Aluno));
     printf("\n");
     printf("========================================================\n");
@@ -74,17 +73,20 @@ Aluno* cadastrar_aluno(void) {
 
 void buscar_aluno(void) {
     system("clear||cls");
+    char cpf[15];
     printf("\n");   //quando o usuário informar o cpf do aluno irá mostrar os dados atrelados a aquelas pessoa,planejo implementar um menu que de outras opções de procura
     printf("========================================================\n");
     printf("    *************** Pesquisar Aluno *************     \n\n");
     printf("                                                        \n");
-    printf("            Informe o CPF(apenas números):              \n");
+    ler_cpf(cpf);
+    procura_aluno(cpf);
     printf("                                                        \n");
     printf("========================================================\n");
     printf("\n");
-    getchar(); printf("Digite enter para continuar...");getchar(); 
+    printf("Digite enter para continuar...");getchar(); 
 
 }
+
 
 void atualizar_aluno(void) {
     system("clear||cls");
@@ -100,17 +102,19 @@ void atualizar_aluno(void) {
     getchar(); printf("Digite enter para continuar...");getchar(); 
 }
 
-void excluir_aluno(void) {
+void excluir_aluno() {
     system("clear||cls");
+    char cpf [15];
     printf("\n");
     printf("========================================================\n");
     printf("    *************** Excluir Aluno *************       \n\n");
     printf("                                                        \n");
-    printf("            Informe o CPF(apenas números):              \n");
+    ler_cpf(cpf);
+    remover_aluno(cpf);
     printf("                                                        \n");
     printf("========================================================\n");
     printf("\n");
-    getchar(); printf("Digite enter para continuar...");getchar(); 
+    printf("Digite enter para continuar...");getchar(); 
 }
 
 
@@ -120,8 +124,8 @@ void relatorio_aluno(void){
     printf("========================================================\n");
     printf("    *************** Relatório Aluno *************       \n\n");
     printf("                                                        \n");
-    printf("       (informar todos os alunos cadastrado)            \n");
-    listaaluno();
+    printf("(informar todos os alunos cadastrado)                   \n");
+    listar_todos_aluno();
     printf("                                                        \n");
     printf("========================================================\n");
     printf("\n");
@@ -138,40 +142,121 @@ void gravaraluno(Aluno* std){
     }
     fwrite(std,sizeof(Aluno),1,fa);
     fclose(fa);
+    free(std);
 }
 
-void exibir_alunos(Aluno* std){
-    char estado[16];
+void exibicao_alunos(Aluno* std){
+    char estado[17];
     if ((std==NULL) || (std->status=='I')){
         printf("\nEste Aluno não existe no sistema!\n");
     }else{
-        printf("********Dados Do Aluno********");
-        printf("\n\tNome:%s",std->nome);
-        printf("\tCPF:%s\n",std->cpf);
-        printf("\tEmail:%s\n",std->email);
-        printf("\tTelefone:%s\n",std->telefone);
+        printf("\n********Dados Do Aluno********");
+        printf("\nNome:%s",std->nome);
+        printf("CPF:%s\n",std->cpf);
+        printf("Email:%s\n",std->email);
+        printf("Telefone:%s\n",std->telefone);
+        printf("status:%d",std->status);
         if(std->status=='M'){
             strcpy(estado,"Matriculado");
-        }else if(std->status=='N'){
-            strcpy(estado,"Não Encontrado");
-        }else{
+        }else if(std->status=='I'){
             strcpy(estado,"Fechado");
         }
     }
 }
 
-void listaaluno(void){
+void listar_todos_aluno(void){
     FILE* fa;
+    Aluno* std;
     std=(Aluno*)malloc(sizeof(Aluno));
     fa=fopen("Alunos.dat","rb");
     if (fa==NULL){
-        printf("Erro na recuperação dos dados!\n");
+        printf("\nNenhum aluno cadastrado!\n");
     }
     while(fread(std,sizeof(Aluno),1,fa)){
         if (std->status!='I'){
-            exibir_alunos(std);
+            exibicao_alunos(std);
         }
     }
     fclose(fa);
     free(std);
 }
+
+void procura_aluno(char cpf[]) {
+    FILE* fa;
+    Aluno* std;
+    std=(Aluno*)malloc(sizeof(Aluno));
+    fa=fopen("Alunos.dat","rb");
+    if (std == NULL) {
+    printf("\tAluno não encontrado!\n");
+        return;
+    }
+    while(fread(std, sizeof(Aluno), 1, fa)) {
+        if ((strcmp(std->cpf, cpf) == 0) && (std->status)) {
+            exibicao_alunos(std);
+        }
+    }
+    fclose(fa);
+    free(std);
+}
+
+void remover_aluno(char cpf[]) {
+    FILE* fa;
+    Aluno std;
+    int encontra = 0;
+    fa = fopen("Alunos.dat", "r+b");
+    if (fa == NULL) {
+        printf("Erro na abertura do arquivo!\n");
+        return;
+    }
+    while (fread(&std, sizeof(Aluno), 1, fa)) {
+        if (strcmp(std.cpf, cpf) == 0 && std.status == 'M') {
+            encontra = 1;
+            std.status = 'I';
+            fseek(fa, -1 * (long)sizeof(Aluno), SEEK_CUR);
+            fwrite(&std, sizeof(Aluno), 1, fa);
+            printf("\nAluno excluído!\n");
+            break; // Encerre o loop após a exclusão
+        }
+    }
+    if (!encontra) {
+        printf("\nAluno não encontrado!\n");
+    }
+    fclose(fa);
+}
+
+// void att_aluno(char cpf[]){
+//     FILE* fa;
+//     Aluno* std;
+//     int encontra=0;
+//     std=(Aluno*)malloc(sizeof(Aluno));
+//     fa=fopen("Alunos.dat","rb");
+//     if (std == NULL) {
+//     printf("\tAluno não encontrado!\n");
+//         return;
+//     }
+//     while (fread(&std, sizeof(Aluno), 1, fa)) {
+//         if (strcmp(std->cpf, cpf) == 0 && std->status == 'M') {
+//             encontra = 1;
+//         printf("========================================================\n");
+//         printf("    *************** Atualizar Aluno *************     \n\n");
+//         printf("                                                        \n");
+//         printf("            o que deseja atualizar?                     \n");
+//         printf("         Telefone(1) - Email(2) - Voltar(0)             \n");
+//         printf("                                                        \n");
+//         scanf("%d",opc);
+//         getchar();
+//         switch (opc) { 
+//             case '1':
+//                 relatorio_aluno();
+//                 break;
+//             case '2':
+//                 relatorio_professor();
+//                 break;
+//             case '0':
+//                 opc=0;
+//                 break;
+//             default:
+//                 printf("\nOpção Inválida!\n");
+//                 getchar();printf("Digite enter para continuar...");getchar(); 
+//                 break;
+        
