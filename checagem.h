52df053483b0b2eb8e40f5a727d16dcd5347b43b
Matheusdnf <matheusdiniz870@gmail.com>
2023-11-06 15:47:18 -0300

    assinatura função

---
+++ checagem.h
@@ -0,0 +1,7 @@
+void listar_todos_diciplina_alt(void);
+int verifica_existe_d(char diciplina[]);
+int verifica_existe_prof_d(char cpf[]);
+void listar_todos_professor_alt(void);
+int verifica_existe_prof(char cpf[]);
+int verifica_existe_aluno(char cpf[]);
+void explicacao(void);
