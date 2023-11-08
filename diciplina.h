5396851e9e02e124b41ee62ef5e4264be47b7256
Matheusdnf <matheusdiniz870@gmail.com>
2023-11-08 20:16:49 -0300

    atualização das assinaturas

--- diciplina.h
+++ diciplina.h
@@ -7,23 +7,23 @@ typedef struct diciplina {
     //char status;
 } Diciplina;
 //tela do módulo diciplina
-void menu_diciplina(void);
-Diciplina* cadastrar_diciplina(void);
-void buscar_diciplina(void);
-void atualizar_diciplina(void);
-void excluir_diciplina(void);
-void relatorio_diciplina(void);
+void menu_diciplinas(void);
+Diciplina* cadastrar_diciplinas(void);
+void buscar_diciplinas(void);
+void atualizar_diciplinas(void);
+void excluir_diciplinas(void);
+void relatorio_diciplinas(void);
 //funções para validar as diciplinas
-int valida_diciplina(char *diciplina);
-void ler_diciplina(char *diciplina);
+int valida_diciplinas(char *diciplina);
+void ler_diciplinas(char *diciplina);
 void diciplinas(void);
 //função de criar id
 int criar_id_d(void);
 //gravação e exibição de dados
-void gravardiciplina(Diciplina* dic);
+void gravardiciplinas(Diciplina* dic);
 //funções do crud
 void exibir_diciplinas(Diciplina* dic);
-void listar_todas_diciplina(void);
-void procura_diciplina(int id);
-void remover_diciplina(int id);
-void att_diciplina(int id);
+void listar_todas_diciplinas(void);
+void procura_diciplinas(int id);
+void remover_diciplinas(int id);
+void att_diciplinas(int id);
