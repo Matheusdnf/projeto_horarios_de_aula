870254556b062ef97aa9a685f69f966557716d0d
Matheusdnf <matheusdiniz870@gmail.com>
2023-11-28 22:30:17 -0300

    cabeçalho

--- checagem.h
+++ checagem.h
@@ -7,4 +7,7 @@ void listar_todos_professor_alt(void);
 int verifica_existe_prof(char cpf[]);
 int verifica_existe_aluno(char cpf[]);
 void explicacao(void);
+int verificar_turma_existente(char cod[]);
+int verifica_aluno_matriculado(char cpf[]);
+void listar_turma_cadastradas_alt(void);
 #endif
